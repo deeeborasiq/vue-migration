a7ffb3bb90c215089f52db9783c316901d1869f1
Débora Siqueira <debora.siqueira@infraclearsalecom.onmicrosoft.com>
2022-11-23 06:46:09 -0300

    feat: adicionando rotas

---
+++ src/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+Vue.use(Router);
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('./pages/Home.vue'),
+  },
+  {
+    path: '/movie/:id',
+    name: 'Movie',
+    component: () => import('./pages/Movie.vue'),
+  }
+]
+const router = new Router({
+  routes,
+}) 
+
+export default router
