7d248770c4c86847c76bf67aa6746a442c76f56b
Matheus Barros <matheusbarros@betalearning.com.br>
2022-11-24 08:18:48 -0300

    feat: adding not found

--- src/router.js
+++ src/router.js
@@ -1,22 +1,27 @@
-import Vue from 'vue';
-import Router from 'vue-router';
+import Vue from "vue";
+import Router from "vue-router";
 
 Vue.use(Router);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import('./pages/Home.vue'),
+    path: "/",
+    name: "Home",
+    component: () => import("./pages/Home.vue"),
   },
   {
-    path: '/movie/:id',
-    name: 'Movie',
-    component: () => import('./pages/Movie.vue'),
-  }
-]
+    path: "/movie/:id",
+    name: "Movie",
+    component: () => import("./pages/Movie.vue"),
+  },
+  {
+    path: "*",
+    name: "NotFound",
+    component: () => import("./pages/NotFound.vue"),
+  },
+];
 const router = new Router({
   routes,
-}) 
+});
 
-export default router
+export default router;
