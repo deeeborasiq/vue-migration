e0e3008e607c3c4847d57cde25c33ef193e08f5c
Débora Siqueira <debora.siqueira@infraclearsalecom.onmicrosoft.com>
2022-11-24 12:24:38 -0300

    feat: filtro de data

--- src/main.js
+++ src/main.js
@@ -4,6 +4,20 @@ import router from './router'
 import "@picocss/pico/css/pico.min.css";
 
 Vue.config.productionTip = false
+
+Vue.filter('formatDate', function (value) {
+  const options = {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+  };
+  if (value) {
+    const date = new Date(value);
+    return new Intl.DateTimeFormat('pt-BR', options).format(date);
+  }
+  return '';
+});
+
 new Vue({
   render: h => h(App),
   router,
