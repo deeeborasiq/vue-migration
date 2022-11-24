e36cd3f72896f1750c02785b2884cf2670c60679
Débora Siqueira <debora.siqueira@infraclearsalecom.onmicrosoft.com>
2022-11-24 16:21:44 -0300

    removendo compat

--- src/main.js
+++ src/main.js
@@ -2,27 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router' 
 import "@picocss/pico/css/pico.min.css";
-/* 
-Vue.config.productionTip = false
 
-Vue.filter('formatDate', function (value) {
-  const options = {
-    day: '2-digit',
-    month: '2-digit',
-    year: 'numeric',
-  };
-  if (value) {
-    const date = new Date(value);
-    return new Intl.DateTimeFormat('pt-BR', options).format(date);
-  }
-  return '';
-});*/
-/* 
-new Vue({
-  render: h => h(App),
-  router,
-}).$mount('#app')
-*/
 const formatDate = (value) => {
   const options = {
     day: '2-digit',
